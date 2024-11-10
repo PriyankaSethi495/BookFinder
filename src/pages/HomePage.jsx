90bf3a38c6f1dc0391ad82a3f695b4c3ed032bbc
p559153 <priyanka@mail.upb.de>
2024-11-10 19:59:15 +0530

    search and display

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react';
+import SearchBar from '../components/SearchBar';
+import '../styles/homepage.css';
 
 const HomePage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [hasSearched, setHasSearched] = useState(false);
 
-export default HomePage
+  // Fetch books based on the search query, page, and language
+  const handleSearch = async (query) => {
+    if (!query) return;
+    setLoading(true);
+    try {
+      const url = `https://openlibrary.org/search.json?title=${query}}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setBooks(data.docs);
+    } catch (error) {
+      console.error("Error fetching books:", error);
+    }
+    setLoading(false);
+    setHasSearched(true);
+  };
+
+  return (
+    <div className="container">
+      <h1>Your Book Finder</h1>
+      <SearchBar onSearch={handleSearch} />
+      {loading ? (<p>Loading</p>):(
+      <div className="book-results">
+        {hasSearched && books.length === 0 ? (
+          <p className="no-results">No books found. Try a different search.</p>
+        ) : (
+          books.map((book) => (
+            <div key={book.key} className="book-card">
+              <h3>{book.title}</h3>
+              <p>{book.author_name?.join(', ')}</p>
+              <p>{book.first_publish_year}</p>
+            </div>
+          ))
+        )}
+      </div>)} 
+    </div>
+  );
+};
+
+export default HomePage;
